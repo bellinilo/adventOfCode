fe41750a14da55621ff6ab642ca3a99e8b90a9cf
Lorenzo Bellini <lorenzo.bellini@emberware.it>
2021-12-04 23:19:23 +0100

    04/2021 first part now search for columns too (and works)

--- 2021/04.php
+++ 2021/04.php
@@ -3,9 +3,10 @@ $list = file_get_contents("04/input.txt");
 $set = prepareSet($list);
 foreach ($set["calls"] as $call_key => $call) {
     call($call, $set["sheets"]);
-    $check = checkSheets($set["sheets"]);
-    if(!empty($check)){
-        $first_count=calculateWinner($check[0], $call);
+    $winner_sheets = checkSheets($set["sheets"]);
+    if(!empty($winner_sheets)){
+        $winner_sheet_value=calculateWinner($winner_sheets[0]);
+        $first_count = $winner_sheet_value * $call;
         break;
     }
 }
@@ -37,7 +38,8 @@ function call($call, &$sheets){
         foreach ($sheet["lines"] as $sheet_line_key => $sheet_line) {
             foreach ($sheet_line as $sheet_line_number_key => $sheet_line_number) {
                 if($sheet_line_number == $call){
-                    $sheets[$sheet_key]["line_calls"][$sheet_line_key][$sheet_line_number_key] = $sheet_line_number;
+                    $sheets[$sheet_key]["row_calls"][$sheet_line_key][$sheet_line_number_key] = $sheet_line_number;
+                    $sheets[$sheet_key]["col_calls"][$sheet_line_number_key][$sheet_line_key] = $sheet_line_number;
                 }
             }
         }
@@ -46,12 +48,11 @@ function call($call, &$sheets){
 
 function checkSheets(&$sheets){
     foreach ($sheets as $sheet_key => $sheet) {
-        if(!array_key_exists("line_calls", $sheet)) continue;
-        $sheet_calls= $sheet["line_calls"];
-        foreach ($sheet["line_calls"] as $sheet_line_key => $sheet_line) {
-            if(count($sheet_line)==5){
-                $results[] = $sheet;
-            }
+        if(!array_key_exists("row_calls", $sheet)) continue;
+        if(!array_key_exists("col_calls", $sheet)) continue;
+        if (checkSheet($sheet["row_calls"]) ||
+            checkSheet($sheet["col_calls"])){
+            $results[] = $sheet;
         }
     }
     if (!empty($results)) {
@@ -60,16 +61,24 @@ function checkSheets(&$sheets){
     return false;
 }
 
-function calculateWinner($sheet, $call){
+function checkSheet($sheet_lines){
+    foreach ($sheet_lines as $sheet_line_key => $sheet_line) {
+        if(count($sheet_line)==5){
+            return true;
+        }
+    }
+    return false;
+}
+
+function calculateWinner($sheet){
     $result=0;
     foreach ($sheet["lines"] as $line_key => $line) {
         foreach ($line as $number_key => $number) {
-            if(!array_key_exists($line_key, $sheet["line_calls"]) ||
-                !array_key_exists($number_key, $sheet["line_calls"][$line_key])){
+            if(!array_key_exists($line_key, $sheet["row_calls"]) ||
+                !array_key_exists($number_key, $sheet["row_calls"][$line_key])){
                 $result += $number;
             }
         }
     }
-    $result *= $call;
     return $result;
 }
